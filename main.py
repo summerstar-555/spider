8e4cffc42ffe3fd5618a3269d4a5e6c71aa1fc52
summerstar-555 <3577295397@qq.com>
2023-01-07 14:57:47 +0800

    111

--- main.py
+++ main.py
@@ -89,61 +89,25 @@ print(r.text)
 # print(nums(con))
 # print(con[0] + con[1])
 
-# 获取wallhaven的图片
-import os
+# 反爬虫，修改UA
+# 导入模块
+"""
 import requests
-from lxml import etree
-import time
 
+url = 'http://httpbin.org/get'  # 在网站后面加了/get,向网站发送请求的命令，这个网站是一个测试网站，不止可以用来看headers
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:65.0) Gecko/20100101 Firefox/65.0'}
+html = requests.get(url, headers=headers).text
+print(html)
+"""
 
-def print_tags():  # 输出标签
-    wallhaven_url = 'https://wallhaven.cc/'
-    tags_xpath = '//a[@class="tagname sfw"]/text()'
-    resp = requests.get(wallhaven_url)
-    element = etree.HTML(resp.text)
-    list1 = element.xpath(tags_xpath)
-    for tag_content in list1:
-        tag_content = '#' + tag_content
-        print(f"\033[0;36;40m{tag_content}\033[0m", end='  ')  # 改变字体的前景色和背景色
+# timeout参数
+# 使用get方法长时间无响应，设置timeout参数
+import requests
 
-
-def image_link_process(image_link: str) -> str:
-    # https://w.wallhaven.cc/full/rd/wallhaven-rddgwm.jpg
-    # https://th.wallhaven.cc/small/rd/rddgwm.jpg
-    if "small" in image_link:
-        image_link = image_link.replace("th", "w")
-        image_link = image_link.replace("small", "full")
-        url_path = image_link.split("/")
-        url_path[-1] = f"wallhaven-{url_path[-1]}"
-        return "/".join(url_path)
-    return ""
-
-
-print_tags()  # 输出标签
-while 1:
-    tag = input("\n请输入想搜索的内容标签：")
-    url = f'https://wallhaven.cc/search?q={tag}'
-    small_xpath = '//img[@alt="loading"]/@data-src'  # 小图片的xpath路径
-    i = 0
-    j = 0
-    r = requests.get(url)
-    e = etree.HTML(r.text)
-    small_list = e.xpath(small_xpath)  # 获取一页小图片的链接
-    pic_path = f'./Pictures/{tag}/'  # 根据用户输入的标签进行创建文件夹
-    if not os.path.exists(pic_path):  # 检查是否存在
-        os.mkdir(pic_path)
-    for link in small_list:
-        full_pic = image_link_process(link)  # 全屏壁纸
-        fullpic_resp = requests.get(full_pic)
-        if not fullpic_resp.status_code == 200:
-            i = i + 1
-            print(f"找不到图片！({i})")
-        else:
-            j = j + 1
-            print(f'下载成功({j})')
-            with open(f'./Pictures/{tag}/%s.jpg' % time.time(), 'wb') as f:  # 使用时间戳命名
-                f.write(fullpic_resp.content)
-    print(f"一共下载了{j}张图片，下载失败的图片一共有{i}张")
-    answer = input("是否退出:")
-    if answer == '是':
-        break
+url = 'https://wallhaven.cc/'
+try:
+    resp = requests.get(url, timeout=0.001)
+    print(resp.status_code)
+except requests.exceptions.ConnectTimeout:
+    print('连接超时')
